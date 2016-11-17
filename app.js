9c47876fc9a404fecc80487b3b522beff3503774
Reinier Sierag <reinier@sierag.nl>
2016-11-17 21:30:53 +0100

    TTN connectie

--- app.js
+++ app.js
@@ -10,37 +10,50 @@ var users = require('./routes/users');
 
 var app = express();
 
+var ttn = require('ttn');
+var appEUI = '70B3D57ED000137B';
+var accessKey = 'CTiL3vk0tsbaxho65xIcouJHeoyyBVYERzCTlJygPOU=';
+var client = new ttn.Client('staging.thethingsnetwork.org', appEUI, accessKey);
+
+client.on('uplink', function (msg) {
+  console.log('Received message', msg);
+});
+
+client.on('activation', function (msg) {
+  console.log('Device activated:', msg.devEUI);
+});
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+//app.use(function(req, res, next) {
+//  var err = new Error('Not Found');
+//  err.status = 404;
+//  next(err);
+//});
 
 // error handler
-app.use(function(err, req, res, next) {
+//app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//  res.locals.message = err.message;
+//  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//  res.status(err.status || 500);
+//  res.render('error');
+//});
 
 module.exports = app;
