1bc1e1137905c3d46c9d12334338f7fee5dfb818
Reinier Sierag <reinier@sierag.nl>
2016-11-18 20:00:43 +0100

    Code cleanup

--- app.js
+++ app.js
@@ -1,59 +1,21 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var curl = require('curlrequest');
 var app = express();
-
 var ttn = require('ttn');
 var appEUI = '70B3D57ED000137B';
 var accessKey = 'CTiL3vk0tsbaxho65xIcouJHeoyyBVYERzCTlJygPOU=';
 var client = new ttn.Client('staging.thethingsnetwork.org', appEUI, accessKey);
 
 client.on('uplink', function (msg) {
-  console.log('Received message', msg);
+	console.log(msg);
+	curl.request({ url: 'http://iot.kaasfabriek.nl/ttnlog/?msg=' + msg }, function (err, stdout, meta) {
+    		
+		console.log('%s %s', meta.cmd, meta.args.join(' '));
+	});
 });
 
 client.on('activation', function (msg) {
   console.log('Device activated:', msg.devEUI);
 });
-
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
-
-//app.use('/', index);
-//app.use('/users', users);
-
-// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  var err = new Error('Not Found');
-//  err.status = 404;
-//  next(err);
-//});
-
-// error handler
-//app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-//  res.locals.message = err.message;
-//  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-//  res.status(err.status || 500);
-//  res.render('error');
-//});
 
 module.exports = app;
