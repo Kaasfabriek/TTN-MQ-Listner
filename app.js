df9c212de0303061ba80215c0dab9fb820494f14
Reinier Sierag <reinier@sierag.nl>
2016-11-19 08:23:19 +0100

    Refractoring code
    Made is better

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+// MODULES
 var express = require('express');
 var curl = require('curlrequest');
 var app = express();
 var ttn = require('ttn');
+
+// CONFIG
 var appEUI = '70B3D57ED000137B';
 var accessKey = 'CTiL3vk0tsbaxho65xIcouJHeoyyBVYERzCTlJygPOU=';
 var client = new ttn.Client('staging.thethingsnetwork.org', appEUI, accessKey);
+var logurl = 'http://iot.kaasfabriek.nl/ttnlog/?msg=';
 
+// SERVER
 client.on('uplink', function (msg) {
+	console.log('Found Message, sending to ' + logurl);
 	console.log(msg);
-	curl.request({ url: 'http://iot.kaasfabriek.nl/ttnlog/?msg=' + msg }, function (err, stdout, meta) {
-    		
-		console.log('%s %s', meta.cmd, meta.args.join(' '));
+	curl.request({ url: logurl + JSON.stringify(msg)}, function (err, stdout, meta) {
+    		console.log('%s %s', meta.cmd, meta.args.join(' '));
 	});
 });
 
